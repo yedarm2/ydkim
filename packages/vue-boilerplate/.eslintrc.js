77ebb3ab03079555ec73a35e54846701c5619f99
yedarm <oncoder@naver.com>
2020-12-06 18:40:33 +0900

    eslint config 수정

--- packages/vue-boilerplate/.eslintrc.js
+++ packages/vue-boilerplate/.eslintrc.js
@@ -1,9 +1,7 @@
-// const path = require('path');
+const path = require('path');
 
-// module.exports = {
-// 	extends: [
-// 		path.resolve(__dirname, '../../.eslintrc.js'),
-// 	],
-// };
-// * 어째서인지 이렇게 해야 동작이 된다...
-module.exports = require('../../.eslintrc.js');
+module.exports = {
+	extends: [
+		path.resolve(__dirname, '../../.eslintrc.js'),
+	],
+};
