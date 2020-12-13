2ffdcba9c36fac7d086d4b0b27205b36da39eb2f
yedarm <oncoder@naver.com>
2020-12-13 22:58:41 +0900

    설정 변경 및 테스트 땜빵으로 수정

--- packages/vue-boilerplate/tests/unit/example.spec.ts
+++ packages/vue-boilerplate/tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/App.vue';
 
-describe('HelloWorld.vue', () => {
+describe('App.vue', () => {
 	it('renders props.msg when passed', () => {
-		const msg = 'new message';
-		const wrapper = shallowMount(HelloWorld, {
-			props: { msg },
-		});
-		expect(wrapper.text()).toMatch(msg);
+		const wrapper = shallowMount(App);
+		expect(wrapper.text()).toMatch('application');
 	});
 });
