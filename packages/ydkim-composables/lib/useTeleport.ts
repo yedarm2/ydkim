1d15949f51ccf794e740a73c24ee742c6f7aa69d
yedarm <oncoder@naver.com>
2020-12-16 22:25:55 +0900

    Fix: useTeleport 수정

--- packages/ydkim-composables/lib/useTeleport.ts
+++ packages/ydkim-composables/lib/useTeleport.ts
@@ -7,16 +7,14 @@ export default (element = 'div', teleportParentSelector = '.teleport') => {
 	console.info(2);
 	const teleportElement = document.createElement(element);
 
-	const teleportSelector = ref(generateRandom.string());
+	const teleportSelector = generateRandom.string();
 
 	teleportParent.appendChild(teleportElement);
-	teleportElement.classList.add(teleportSelector.value);
+	teleportElement.classList.add(teleportSelector);
 
 	onUnmounted(() => {
 		teleportParent.removeChild(teleportElement);
 	});
 
-	return {
-		teleportSelector,
-	};
+	return ref(`.${teleportSelector}`);
 };
