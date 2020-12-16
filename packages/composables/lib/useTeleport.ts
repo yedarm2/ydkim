174f21c5904f2189e3766c8ebc80f01da94d18a3
yedarm <oncoder@naver.com>
2020-12-16 23:54:43 +0900

    Todo 주석 추가

--- packages/composables/lib/useTeleport.ts
+++ packages/composables/lib/useTeleport.ts
@@ -1,6 +1,7 @@
 import { ref, onUnmounted } from 'vue';
 import { generateRandom } from '@ydkim/utils';
 
+// TODO: 추후에 readonly로 감싸서 반환하는 방법을 테스트해보자.
 export default (element = 'div', teleportParentSelector = '.teleport') => {
 	console.info(1);
 	const teleportParent = document.querySelector(teleportParentSelector) as HTMLElement;
