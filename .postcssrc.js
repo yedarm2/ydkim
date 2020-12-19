3b96333c1ac901851babf1fe6284b495486e71ee
yedarm <oncoder@naver.com>
2020-12-19 17:01:06 +0900

    vue 3 storybook 우선 적용 완료(?)

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,8 +3,9 @@ module.exports = {
 		require('stylelint'),
 		require('autoprefixer'),
 		require('postcss-preset-env'),
-		require('postcss-reporter')({
-			clearReportedMessages: true,
-		}),
+		// * 무슨 이유에서인지 cannot find module 에러가 발생한다. ㅠㅠ
+		// require('postcss-reporter')({
+		// 	clearReportedMessages: true,
+		// }),
 	],
 };
