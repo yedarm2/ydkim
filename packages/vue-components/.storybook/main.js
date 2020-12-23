ade90a599c4e870f066b566639008677578d217b
yedarm <oncoder@naver.com>
2020-12-24 00:14:39 +0900

    storybook 설정 변경

--- packages/vue-components/.storybook/main.js
+++ packages/vue-components/.storybook/main.js
@@ -5,4 +5,12 @@ module.exports = {
 		'../src/components/**/*.stories.@(js|jsx|ts|tsx)',
 	],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	webpackFinal(config) {
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: ['vue-style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+		});
+
+		return config;
+	},
 };
