5be566dc620b6d1f76142cd2b732e56d1e8c79c1
yedarm <oncoder@naver.com>
2021-07-12 23:52:15 +0900

    코드 수정

--- packages/react-tutorial/src/Counter.js
+++ packages/react-tutorial/src/Counter.js
@@ -4,17 +4,18 @@ function Counter() {
 	const [number, setNumber] = useState(0);
 
 	const onIncrease = () => {
-		setNumber(number => number + 1);
+		setNumber(number + 1);
 	};
+
 	const onDecrease = () => {
-		setNumber(number => number - 1);
+		setNumber(number - 1);
 	};
 
 	return (
 		<div>
-			{/* <h1>{number}</h1> */}
-			{/* <button onClick={onIncrease}>+1</button> */}
-			{/* <button onClick={onDecrease}>-1</button> */}
+			<h1>{number}</h1>
+			<button onClick={onIncrease}>+1</button>
+			<button onClick={onDecrease}>-1</button>
 		</div>
 	);
 }
