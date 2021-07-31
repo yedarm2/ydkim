7e1aa1409f51b6c0b9349260bbe3ca09a30448f0
yedarm <oncoder@naver.com>
2021-07-31 11:35:44 +0900

    리액트 공부

--- packages/react-tutorial/src/api-intergrate/Users.js
+++ packages/react-tutorial/src/api-intergrate/Users.js
@@ -1,67 +1,35 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-function reducer(state, action) {
-	console.info(state, action);
-	switch (action.type) {
-		case 'LOADING':
-			return {
-				loading: true,
-				users: null,
-				error: null,
-			};
-		case 'SUCCESS':
-			return {
-				loading: false,
-				users: action.data,
-				error: null,
-			};
-		case 'ERROR':
-			return {
-				loading: false,
-				users: null,
-				error: action.error,
-			};
-		default:
-			throw new Error(`Unhandled action type: ${action.type}`);
-	}
+import User from './User';
+
+import useAsync from './useAsync';
+
+async function getUsers() {
+	const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+	return response.data;
 }
 
 function Users() {
-	const [{ loading, users, error }, dispatch] = useReducer(reducer, {
-		loading: false,
-		users: null,
-		error: null,
-	});
-
-	const fetchUsers = async () => {
-		dispatch({ type: 'LOADING' });
-		try {
-			const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-			dispatch({ type: 'SUCCESS', data: response.data });
-		} catch (error) {
-			dispatch({ type: 'ERROR', error });
-		}
-	};
-
-	useEffect(() => {
-		fetchUsers();
-	}, []);
+	const [{ loading, error, data: users }, reFetch] = useAsync(getUsers, [], true);
+	const [userId, setUserId] = useState(null);
 
 	if (loading) return <div>로딩중..</div>;
 	if (error) return <div>에러가 발생했습니다.</div>;
-	if (!users) return null;
+	if (!users) return <button onClick={reFetch}>불러오기</button>;
 
 	return (
 		<>
 			<ul>
 				{users.map(user => (
-					<li key={user.id}>
+					<li key={user.id} onClick={() => setUserId(user.id)}>
 						{user.username} ({user.name})
 					</li>
 				))}
 			</ul>
-			<button onClick={fetchUsers}>다시 불러오기</button>
+			<button onClick={reFetch}>다시 불러오기</button>
+			{userId && <User id={userId} />}
 		</>
 	);
 }
