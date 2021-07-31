bdf0e493d2c0b4bc0f6f69480dd207b6ac05344c
yedarm <oncoder@naver.com>
2021-07-31 12:31:38 +0900

    react-async 적용 공부

--- packages/react-tutorial/src/api-intergrate/User.js
+++ packages/react-tutorial/src/api-intergrate/User.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 
-async function getUser(id) {
+// import useAsync from './useAsync';
+
+async function getUser({ id }) {
 	const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
 
 	return response.data;
 }
 
 function User({ id }) {
-	const [{ loading, data: user, error }] = useAsync(() => getUser(id), [id]);
+	const {
+		isLoading,
+		data: user,
+		error,
+	} = useAsync({
+		promiseFn: getUser,
+		id,
+		watch: id,
+	});
 
-	if (loading) return <div>로딩중..</div>;
+	if (isLoading) return <div>로딩중..</div>;
 	if (error) return <div>에러가 발생했습니다.</div>;
 	if (!user) return null;
 
