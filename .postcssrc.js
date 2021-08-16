5089f38379bef805d5aa80fd4559338f915e0f83
yedarm <oncoder@naver.com>
2021-08-16 22:12:35 +0900

    postcss config 수정

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-	plugins: [
-		require('stylelint'),
-		require('autoprefixer'),
-		require('postcss-preset-env'),
+	plugins: {
+		'stylelint': {},
+		'autoprefixer': {},
+		'postcss-preset-env': {},
 		// * 무슨 이유에서인지 cannot find module 에러가 발생한다. ㅠㅠ
-		// require('postcss-reporter')({
+		// 'postcss-reporter': {
 		// 	clearReportedMessages: true,
-		// }),
-	],
+		// },
+	},
 };
