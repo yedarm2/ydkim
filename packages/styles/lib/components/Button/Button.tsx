09b3dac2fa055189a4f91bc800d2b14830f9f63e
yedarm <oncoder@naver.com>
2021-10-03 23:07:10 +0900

    style(react)를 위한 패키지 추가

---
+++ packages/styles/lib/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export interface ButtonProps {}
+
+const Button = styled.button<ButtonProps>``;
+
+export default Button;
---
+++ packages/styles/lib/index.ts
@@ -0,0 +1,7 @@
+import Button, { ButtonProps } from './components/Button/Button';
+import * as mixins from './emotion/mixins';
+import * as variables from './emotion/variables';
+
+export type { ButtonProps };
+
+export { Button, mixins, variables };
