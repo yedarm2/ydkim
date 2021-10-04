40a3eb47a96d9b9a7bcf508f6adc1c4c8d4a0b7d
yedarm <oncoder@naver.com>
2021-10-04 19:26:14 +0900

    storybook 적용

---
+++ packages/styles/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+	stories: [
+		'../stories/**/*.stories.mdx',
+		'../stories/**/*.stories.@(js|jsx|ts|tsx)',
+		'../lib/components/**/*.stories.mdx',
+		'../lib/components/**/*.stories.@(js|jsx|ts|tsx)',
+	],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+};
