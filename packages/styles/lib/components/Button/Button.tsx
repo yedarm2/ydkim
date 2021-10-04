b599caa0ea1e8dfc564146812359efd53d405434
yedarm <oncoder@naver.com>
2021-10-04 23:27:58 +0900

    코드 수정

--- packages/styles/lib/components/Button/Button.tsx
+++ packages/styles/lib/components/Button/Button.tsx
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 
 export interface ButtonProps {}
 
-const Button = styled.button<ButtonProps>``;
+const Button = styled.button<ButtonProps>`
+	cursor: pointer;
+`;
 
 export default Button;
