cb292aa8c64e0a13fbb9ba92dd018b6a015fa343
yedarm <oncoder@naver.com>
2022-01-08 23:04:09 +0900

    color 백그라운드 파라미터 추가

--- packages/react-components/.storybook/preview.js
+++ packages/react-components/.storybook/preview.js
@@ -1,5 +1,7 @@
 // import '@ydkim/styles';
 
+import { colors } from '../src/variables';
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	controls: {
@@ -7,5 +9,8 @@ export const parameters = {
 			color: /(background|color)$/i,
 			date: /Date$/,
 		},
+	},
+	backgrounds: {
+		values: Object.entries(colors).map(([name, value]) => ({ name, value })),
 	},
 };
