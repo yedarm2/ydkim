5c0fcd2e1c2bdd8d5847e73c51e1d178567abf75
yedarm <oncoder@naver.com>
2022-02-04 16:27:34 +0900

    id 생성 유틸  추가

--- packages/utils/lib/generateRandom.ts
+++ packages/utils/lib/generateRandom.ts
@@ -1,3 +1,22 @@
+const createUniqueGenerator = <Type>(generator: () => Type): (() => Type) => {
+	const valueSet = new Set<Type>();
+	const getValue = () => {
+		const result = generator();
+
+		if (!valueSet.has(result)) {
+			valueSet.add(result);
+			return result;
+		}
+
+		return getValue();
+	};
+
+	return () => getValue();
+};
+
 export const number = (min = 0, max = 1) => Math.floor(Math.random() * max - min) + min;
 
 export const string = (stringLength = 11) => Math.random().toString(36).substr(2, stringLength);
+
+const uniqueIdGenerator = createUniqueGenerator(() => string());
+export const idString = () => uniqueIdGenerator();
