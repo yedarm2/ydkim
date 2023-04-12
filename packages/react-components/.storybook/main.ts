95eedf724f6d47ff3237eef397f982f5a3352d12
yedarm <jjkg2000kr@gmail.com>
2023-04-12 11:12:11 +0900

    디펜던시 업데이트, storybook vite

---
+++ packages/react-components/.storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from '@storybook/react-vite'; // your framework
+
+const config: StorybookConfig = {
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+		'@storybook/addon-mdx-gfm',
+	],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
+	docs: {
+		autodocs: true,
+	},
+};
+
+export default config;
