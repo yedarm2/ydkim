ee46dc14e726b7e272257e330694447e49bcc3b6
yedarm <oncoder@naver.com>
2023-11-18 19:41:59 +0900

    학교 생성 성공시 생성된 학교로 이동

--- apps/blue-archive-web/src/actions/form/index.ts
+++ apps/blue-archive-web/src/actions/form/index.ts
@@ -1,8 +1,10 @@
 'use server';
 import { schoolService } from '@ydkim/core-service';
 import { getSchoolPayload } from './mapper';
+import { redirect } from 'next/navigation';
 
 export const createSchool = async (formData: FormData) => {
 	const payload = getSchoolPayload(formData);
-	await schoolService.createSchool(payload);
+	const school = await schoolService.createSchool(payload);
+	redirect(`/school/${school.id}`);
 };
