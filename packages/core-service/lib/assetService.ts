f60f048f77194540d0f19f7873cfed41c704526d
yedarm <oncoder@naver.com>
2023-11-18 18:57:45 +0900

    asset service에서 db 저장 실패시 파일 삭제하는 로직 추가

--- packages/core-service/lib/assetService.ts
+++ packages/core-service/lib/assetService.ts
@@ -2,16 +2,16 @@ import { AssetDao, assetDao } from '@ydkim/core-database';
 import { CloudStorage, cloudStorage } from '@ydkim/server-utils';
 
 export class AssetService {
-	assetDao: AssetDao;
-	cloudStorage: CloudStorage;
+	private assetDao: AssetDao;
+	private cloudStorage: CloudStorage;
 
 	constructor() {
 		this.assetDao = assetDao;
 		this.cloudStorage = cloudStorage;
 	}
 
-	async uploadAsset(file: File) {
-		const fileUrl = await this.cloudStorage.uploadFile(file);
+	async uploadAsset(file: File, folderPath?: string) {
+		const fileUrl = await this.cloudStorage.uploadFile(file, folderPath);
 
 		try {
 			return await this.assetDao.create({
@@ -22,6 +22,7 @@ export class AssetService {
 				'업로드한 파일 정보 저장 도중에 에러가 발생했습니다. 업로드한 파일을 삭제하겠습니다.',
 				error,
 			);
+			await this.cloudStorage.deleteFile(file.name, folderPath);
 			throw error;
 		}
 	}
