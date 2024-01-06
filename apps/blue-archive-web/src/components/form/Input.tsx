0579212321e87ee6e16596152ef802883580296b
yedarm <oncoder@naver.com>
2024-01-06 22:51:44 +0900

    input 코드 수정

--- apps/blue-archive-web/src/components/form/Input.tsx
+++ apps/blue-archive-web/src/components/form/Input.tsx
@@ -1,38 +1,18 @@
 'use client';
-import { HTMLInputTypeAttribute, forwardRef } from 'react';
-import { SetState } from '@/types/react';
+import { InputHTMLAttributes, forwardRef } from 'react';
 import { inputStyle } from './Input.css';
 import { classNames } from '@ydkim/browser-utils';
 
-export interface InputProps {
-	id?: string;
-	type?: HTMLInputTypeAttribute;
-	name?: string;
-	value?: string;
-	placeholder?: string;
-	setValue?: SetState<string>;
-	setFile?: SetState<File>;
-	required?: boolean;
-}
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-	({ type = 'text', name, placeholder, value, setValue, setFile, ...props }, elementRef) => {
+	({ type = 'text', value, ...props }, elementRef) => {
 		return (
 			<input
 				ref={elementRef}
-				type={type}
-				name={name}
-				placeholder={placeholder}
 				value={value}
-				// onInput={event => setValue?.((event.target as HTMLInputElement).value)}
-				onInput={event => {
-					setValue?.((event.target as HTMLInputElement).value);
-
-					if (type === 'file') {
-						setFile?.((event.target as HTMLInputElement).files?.[0] as File);
-					}
-				}}
-				className={classNames(inputStyle)}
+				type={type}
+				className={classNames(inputStyle, props.className)}
 				{...props}
 			/>
 		);
