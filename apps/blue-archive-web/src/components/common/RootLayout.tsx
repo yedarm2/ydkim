26af18cf4d727409a38b0fb50737405cfcfaa6e8
yedarm <oncoder@naver.com>
2024-01-07 23:48:33 +0900

    noto sans 폰트 적용

--- apps/blue-archive-web/src/components/common/RootLayout.tsx
+++ apps/blue-archive-web/src/components/common/RootLayout.tsx
@@ -1,11 +1,19 @@
 'use client';
 import { PropsWithChildren } from 'react';
+import { Noto_Sans_KR } from 'next/font/google';
+import { classNames } from '@ydkim/browser-utils';
 
-import { layoutContainer, rootLayoutStyle } from './RootLayout.css';
+import { rootLayoutStyle, layoutContainer } from './RootLayout.css';
+
+const notoSansFontStyle = Noto_Sans_KR({
+	weight: ['400', '500', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+});
 
 export const RootLayout = ({ children }: PropsWithChildren) => {
 	return (
-		<main className={rootLayoutStyle}>
+		<main className={classNames(rootLayoutStyle, notoSansFontStyle.className)}>
 			<section className={layoutContainer}>{children}</section>
 		</main>
 	);
