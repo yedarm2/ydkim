9d5889f48a944e7cabedd8ee79311368fd158152
yedarm <oncoder@naver.com>
2024-01-11 23:46:11 +0900

    next 관련 타입 파일 추가

---
+++ apps/blue-archive-web/src/types/next.ts
@@ -0,0 +1,9 @@
+export interface NextPageParams<Params = unknown, SearchParams = unknown> {
+	params: Params;
+	searchParams: SearchParams;
+}
+
+export interface ErrorPageComponentProps {
+	error: Error & { digest?: string };
+	reset: () => void;
+}
