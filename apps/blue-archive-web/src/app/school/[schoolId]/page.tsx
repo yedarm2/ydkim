6cc0c79748841448d0e5e6dd46d4e9d07b0903af
yedarm <oncoder@naver.com>
2024-01-11 23:21:01 +0900

    school form url 수정

--- apps/blue-archive-web/src/app/school/[schoolId]/page.tsx
+++ apps/blue-archive-web/src/app/school/[schoolId]/page.tsx
@@ -2,6 +2,7 @@ import { SchoolView } from '@/components/school/form/SchoolView';
 
 const SchoolViewPage = async (props: { params: { schoolId: string } }) => {
 	const { schoolId: schoolIdString } = props.params;
+
 	return <SchoolView schoolId={parseInt(schoolIdString)} />;
 };
 
