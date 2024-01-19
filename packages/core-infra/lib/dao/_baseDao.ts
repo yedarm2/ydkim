e162e42e09154921cd554344f81cbd29421b01ab
yedarm <oncoder@naver.com>
2024-01-19 23:46:24 +0900

    prisma sql log를 찍는 코드 추가

--- packages/core-infra/lib/dao/_baseDao.ts
+++ packages/core-infra/lib/dao/_baseDao.ts
@@ -4,6 +4,12 @@ export const prismaClient = new PrismaClient({
 	log: ['info', 'warn', 'error', { emit: 'event', level: 'query' }],
 });
 
+prismaClient.$on('query', event => {
+	console.log('Query: ' + event.query);
+	console.log('Params: ' + event.params);
+	console.log('Duration: ' + event.duration + 'ms');
+});
+
 export class BaseDao {
 	protected prismaClient: PrismaClient;
 
