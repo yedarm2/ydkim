8885f623ca3f8908091b3833047e6f2c3563bab0
yedarm <oncoder@naver.com>
2024-04-07 21:59:19 +0900

    불필요한 코드 제거

--- apps/blue-archive-web/src/shared/ui/common/Button.tsx
+++ apps/blue-archive-web/src/shared/ui/common/Button.tsx
@@ -5,5 +5,3 @@ import { classNames } from '@ydkim/browser-utils';
 export const Button = ({ className, ...buttonProps }: HTMLAttributes<HTMLButtonElement>) => {
 	return <button className={classNames(buttonStyle, className)} {...buttonProps} />;
 };
-
-Button.style = buttonStyle;
